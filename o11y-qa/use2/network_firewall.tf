bdc94bb3ba9072b8b26040f8b77e8714cac973f3
Daniel Furman <djfurman@gmail.com>
2025-11-08 13:04:42 -0500

    Correct incorrect elements in network firewall

--- o11y-qa/use2/network_firewall.tf
+++ o11y-qa/use2/network_firewall.tf
@@ -1,16 +1,16 @@
 resource "aws_networkfirewall_firewall" "o11y_qa_firewall" {
   name                   = "o11y-qa-firewall"
-  firewall_policy_arn    = aws_network_firewall_firewall_policy.o11y_qa_firewall_policy.arn
+  firewall_policy_arn    = aws_networkfirewall_firewall_policy.o11y_qa_firewall_policy.arn
   vpc_id                 = aws_vpc.main.id
   enabled_analysis_types = ["HTTP_HOST"]
 
-  subnet_mappings {
+  subnet_mapping {
     subnet_id = aws_subnet.inspection_use2a.id
   }
-  subnet_mappings {
+  subnet_mapping {
     subnet_id = aws_subnet.inspection_use2b.id
   }
-  subnet_mappings {
+  subnet_mapping {
     subnet_id = aws_subnet.inspection_use2c.id
   }
 
@@ -23,8 +23,10 @@ resource "aws_networkfirewall_firewall_policy" "o11y_qa_firewall_policy" {
   name = "o11y-qa-firewall-policy"
 
   firewall_policy {
-    stateful_rule_group_references {
-      resource_arn = aws_network_firewall_rule_group.o11y_honeycomb_rule_group.arn
+    stateless_default_actions = ["aws:pass"]
+    stateless_fragment_default_actions = ["aws:drop"]
+    stateful_rule_group_reference {
+      resource_arn = aws_networkfirewall_rule_group.o11y_honeycomb_rule_group.arn
     }
   }
 
@@ -40,7 +42,7 @@ resource "aws_networkfirewall_rule_group" "o11y_honeycomb_rule_group" {
 
   rule_group {
     rules_source {
-      rule_source_list {
+      rules_source_list {
         generated_rules_type = "ALLOWLIST"
         target_types = ["HTTP_HOST"]
         targets = ["api.honeycomb.io"]
