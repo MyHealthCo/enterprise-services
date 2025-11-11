f258189d470370d09ab70c38d9e8e881f8905b31
Daniel Furman <djfurman@gmail.com>
2025-11-11 09:57:20 -0500

    Add ServiceEndpoint-SGs. Add Acceptable Usage Tags to Security Groups.

--- o11y-qa/use2/security_groups.tf
+++ o11y-qa/use2/security_groups.tf
@@ -1,11 +1,12 @@
 resource "aws_security_group" "lb_sg" {
   provider    = aws.use2
   name        = "LoadBalancer-SG"
-  description = "Security group for PrivateLink load balancer"
+  description = "Security group for PrivateLink Load Balancer"
   vpc_id      = aws_vpc.main.id
 
   tags = {
     Name = "LoadBalancer-SG"
+    AllowedUsage = "LoadBalancers,ServiceProviders"
   }
 }
 
@@ -17,7 +18,20 @@ resource "aws_security_group" "collector_sg" {
 
   tags = {
     Name = "Collector-SG"
+    AllowedUsage = "Processing"
   }
+}
+
+resource "aws_security_group" "service_endpoint_sg" {
+    provider = aws.use2
+    name = "ServiceEndpoint-SG"
+    description = "Security group for AWS Service Endpoints"
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+      Name = "ServiceEndpoint-SG"
+      AllowedUsage = "ServiceEndpoints"
+    }
 }
 
 resource "aws_security_group_rule" "allow_lb_otlp_inbound" {
@@ -28,15 +42,15 @@ resource "aws_security_group_rule" "allow_lb_otlp_inbound" {
   protocol          = "tcp"
   security_group_id = aws_security_group.lb_sg.id
   cidr_blocks       = ["0.0.0.0/0"]
-  description       = "Allow inbound TLS traffic"
+  description       = "Allow inbound TLS traffic from PrivateLink"
 }
 
 
-resource "aws_security_group_rule" "allow_lb_otlp_outbound" {
+resource "aws_security_group_rule" "allow_lb_otlp_to_tg" {
   provider                 = aws.use2
   type                     = "egress"
-  from_port                = 9990
-  to_port                  = 9990
+  from_port                = 4317
+  to_port                  = 4317
   protocol                 = "tcp"
   security_group_id        = aws_security_group.lb_sg.id
   source_security_group_id = aws_security_group.collector_sg.id
@@ -47,15 +61,15 @@ resource "aws_security_group_rule" "allow_lb_otlp_outbound" {
 resource "aws_security_group_rule" "allow_collector_otlp_inbound" {
   provider                 = aws.use2
   type                     = "ingress"
-  from_port                = 9990
-  to_port                  = 9990
+  from_port                = 4317
+  to_port                  = 4317
   protocol                 = "tcp"
   security_group_id        = aws_security_group.collector_sg.id
   source_security_group_id = aws_security_group.lb_sg.id
   description              = "Allow inbound OTel traffic from LoadBalancer-SG"
 }
 
-resource "aws_security_group_rule" "allow_collector_otlp_outbound" {
+resource "aws_security_group_rule" "allow_collector_otlp_to_aggregator" {
   provider          = aws.use2
   type              = "egress"
   from_port         = 443
@@ -64,4 +78,26 @@ resource "aws_security_group_rule" "allow_collector_otlp_outbound" {
   security_group_id = aws_security_group.collector_sg.id
   cidr_blocks       = ["0.0.0.0/0"]
   description       = "Allow outbound OTLP traffic"
+}
+
+resource "aws_security_group_rule" "allow_compute_https_to_service_endpoint" {
+  provider                 = aws.use2
+  type                     = "egress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.compute_sg.id
+  source_security_group_id = aws_security_group.service_endpoint_sg.id
+  description              = "Allow outbound HTTPS traffic to Service Endpoints"
+}
+
+resource "aws_security_group_rule" "allow_service_endpoint_https_inbound" {
+  provider          = aws.use2
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.service_endpoint_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow inbound HTTPS traffic to Service Endpoints"
 }
