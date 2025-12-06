8c8486015f41d6270a5a5d72d07f2c8183bc3723
Daniel Furman <djfurman@gmail.com>
2025-12-06 17:19:46 -0500

    Apply format changes.

--- connectivity/consumer/use2/subnets.tf
+++ connectivity/consumer/use2/subnets.tf
@@ -11,25 +11,25 @@ resource "aws_subnet" "internal_a" {
 }
 
 resource "aws_subnet" "internal_b" {
-  provider = aws.use2
-  vpc_id = aws_vpc.main.id
+  provider          = aws.use2
+  vpc_id            = aws_vpc.main.id
   availability_zone = "us-east-2b"
   cidr_block        = cidrsubnets(aws_vpc_ipv4_cidr_block_association.usable_cidr, 2, 2, 2)[1]
 
   tags = {
-    Name = "internal-b"
+    Name    = "internal-b"
     Purpose = "Internal"
   }
 }
 
 resource "aws_subnet" "internal_c" {
-  provider = aws.use2
-  vpc_id = aws_vpc.main.id
+  provider          = aws.use2
+  vpc_id            = aws_vpc.main.id
   availability_zone = "us-east-2c"
   cidr_block        = cidrsubnets(aws_vpc_ipv4_cidr_block_association.usable_cidr, 2, 2, 2)[2]
 
   tags = {
-    Name = "internal-c"
+    Name    = "internal-c"
     Purpose = "Internal"
   }
 }
