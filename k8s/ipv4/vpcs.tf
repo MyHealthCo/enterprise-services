de144f7ff2da35cb0914dd4fbf80475068781936
Daniel Furman <djfurman@gmail.com>
2025-12-23 12:30:21 -0500

    Add k8s experiment with an IPv4 only application VPC.

---
+++ k8s/ipv4/vpcs.tf
@@ -0,0 +1,22 @@
+resource "aws_vpc" "main" {
+  provider             = aws.use2
+  cidr_block           = var.cidr_primary
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "k8s-ipv4"
+  }
+}
+
+resource "aws_vpc_ipv4_cidr_block_association" "usable" {
+  provider   = aws.use2
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.cidr_usable
+}
+
+resource "aws_vpc_ipv4_cidr_block_association" "service_endpoint" {
+  provider   = aws.use2
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.cidr_service_endpoint
+}
