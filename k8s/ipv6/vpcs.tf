69c0c6f5db2013f1dd965a5c9a04efd9e2a507e6
Daniel Furman <djfurman@gmail.com>
2025-12-23 12:31:12 -0500

    Add k8s experiment with an IPv6 DualStacked application VPC with additional IPv6 native subnets.

---
+++ k8s/ipv6/vpcs.tf
@@ -0,0 +1,31 @@
+resource "aws_vpc" "main" {
+  provider             = aws.use2
+  cidr_block           = var.cidr_primary
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  ipv6_netmask_length = 56
+
+  tags = {
+    Name = "k8s-ipv6"
+  }
+}
+
+resource "aws_vpc_ipv4_cidr_block_association" "usable" {
+  provider   = aws.use2
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.cidr_usable
+}
+
+resource "aws_vpc_ipv4_cidr_block_association" "service_endpoint" {
+  provider   = aws.use2
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.cidr_service_endpoint
+}
+
+resource "aws_vpc_ipv6_cidr_block_association" "main" {
+  provider = aws.use2
+  vpc_id   = aws_vpc.main.id
+
+  assign_generated_ipv6_cidr_block = true
+}
