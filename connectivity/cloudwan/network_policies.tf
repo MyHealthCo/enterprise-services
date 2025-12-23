1b3a7c81a702f1ce66308e25b370bdaf58300397
Daniel Furman <djfurman@gmail.com>
2025-12-23 12:46:42 -0500

    Fixed network policy document to configure the egress VPC.

--- connectivity/cloudwan/network_policies.tf
+++ connectivity/cloudwan/network_policies.tf
@@ -1,10 +1,12 @@
 data "aws_networkmanager_core_network_policy_document" "main" {
+  provider = aws.use2
+
   core_network_configuration {
     asn_ranges = ["4200000000-4294967294"]
 
     edge_locations {
       location = "us-east-2"
-      asn      = "4200000000"
+      asn      = "4200000102"
     }
   }
 
@@ -21,13 +23,31 @@ data "aws_networkmanager_core_network_policy_document" "main" {
   segments {
     name                          = "null"
     description                   = "Blackhole segment for unwanted traffic"
-    require_attachment_acceptance = true
+    require_attachment_acceptance = false
   }
 
   network_function_groups {
     name                          = "secure-egress-qa"
     description                   = "Network Function Group for QA egress traffic with inspection"
     require_attachment_acceptance = true
+  }
+
+  segment_actions {
+    action      = "send-to"
+    description = "Ships all QA outbound traffic out through the secure egress Network Function Group"
+    segment     = "qa"
+
+    via {
+      network_function_groups = ["secure-egress-qa"]
+
+      with_edge_override {
+        edge_sets = [
+          ["us-east-2"]
+        ]
+
+        use_edge_location = "us-east-2"
+      }
+    }
   }
 
   attachment_policies {
@@ -64,21 +84,33 @@ data "aws_networkmanager_core_network_policy_document" "main" {
     }
   }
 
-  segment_actions {
-    action       = "send-to"
-    description  = "Ships traffic to a black hole from when nothing can escape"
-    destinations = ["blackhole"]
-    segment      = "null"
-  }
-  segment_actions {
-    action      = "send-via"
-    segment     = "qa"
-    description = "Route QA traffic through secure egress Network Function Group"
-    mode        = "attachment-route"
-
-    via {
-      network_function_groups = ["secure-egress-qa"]
+  attachment_policies {
+    rule_number     = 500
+    condition_logic = "or"
+    conditions {
+      type     = "tag-value"
+      operator = "equals"
+      key      = "NetworkFunctionGroup"
+      value    = "secure-egress-qa"
+    }
+    action {
+      add_to_network_function_group = "secure-egress-qa"
     }
   }
 
+  attachment_policies {
+    rule_number     = 999
+    condition_logic = "or"
+
+    conditions {
+      type     = "tag-value"
+      operator = "equals"
+      key      = "NetworkSegment"
+      value    = "null"
+    }
+    action {
+      association_method = "constant"
+      segment            = "null"
+    }
+  }
 }
